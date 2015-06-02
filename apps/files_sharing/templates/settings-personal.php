4e9426bdc305438869d5d2484a687285db304c6f
Bjoern Schiessle <schiessle@owncloud.com>
2015-06-02 19:26:53 +0200

    add translations

--- apps/files_sharing/templates/settings-personal.php
+++ apps/files_sharing/templates/settings-personal.php
@@ -17,7 +17,7 @@ style('files_sharing', '3rdparty/gs-share/style');
 
 		<p>
 
-		<h3>Share your Federated Cloud Id:</h3>
+		<h3><?php p($l->t('Share your Federated Cloud Id:')); ?></h3>
 
 		<p>
 
@@ -43,7 +43,7 @@ style('files_sharing', '3rdparty/gs-share/style');
 
 		<br/>
 
-		<h3>Add your Federated Cloud Id to your homepage:</h3>
+		<h3><?php p($l->t('Add your Federated Cloud Id to your homepage:')); ?></h3>
 
 		<p>
 
@@ -53,7 +53,7 @@ style('files_sharing', '3rdparty/gs-share/style');
 			</a>
 
 			<br/>
-			HTML Code:
+			<?php p($l->t('HTML Code:')); ?>
 			<br/>
 
 			<xmp>
